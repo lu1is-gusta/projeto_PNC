2ef0ecabdcceaeaa51ff64a4151eaf0b72f3305d
lu1is-gusta <gustavovacodar@gmail.com>
2025-03-27 21:20:43 -0300

    end of project

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.querySelector(".btn-sim").addEventListener("click", function() {
+    window.location.href = "../pages/pag4.html";
+});
+
+document.querySelector(".btn-nao").addEventListener("click", function() {
+    window.location.href = "../pages/pag5.html";
+});
